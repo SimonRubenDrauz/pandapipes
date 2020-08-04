d88f3b836cdcf7d8613c76c3332f9f079073eea1
sdrauz <Simon.Ruben.Drauz@iee.fraunhofer.de>
2020-08-04 16:31:14 +0200

    introducing only one pipeflow_comparison.py for all tools in tests

--- pandapipes/test/comparison/openmodelica_comparison/test_water_openmodelica.py
+++ pandapipes/test/comparison/openmodelica_comparison/test_water_openmodelica.py
@@ -7,8 +7,8 @@ import pytest
 import numpy as np
 import pandapipes.networks.simple_water_networks as nw
 from pandapipes.pipeflow import logger as pf_logger
-from pandapipes.test.openmodelica_comparison.pipeflow_openmodelica_comparison \
-    import pipeflow_openmodelica_comparison
+from pandapipes.test.comparison.pipeflow_comparison \
+    import pipeflow_comparison
 
 try:
     import pplog as logging
@@ -24,13 +24,13 @@ pf_logger.setLevel(logging.WARNING)
 # mixed_net
 def test_case_combined_mixed_pc(log_results=False):
     net = nw.water_combined_mixed()
-    p_diff, v_diff_abs = pipeflow_openmodelica_comparison(net, log_results)
+    p_diff, v_diff_abs = pipeflow_comparison(net, 'om', log_results, friction_model='colebrook')
     assert np.all(p_diff < 0.01)
     assert np.all(v_diff_abs < 0.05)
 
 def test_case_combined_mixed_sj(log_results=False):
     net = nw.water_combined_mixed(method="swamee-jain")
-    p_diff, v_diff_abs = pipeflow_openmodelica_comparison(net, log_results)
+    p_diff, v_diff_abs = pipeflow_comparison(net, 'om', log_results, friction_model='colebrook')
     assert np.all(p_diff < 0.01)
     assert np.all(v_diff_abs < 0.05)
 
@@ -38,13 +38,13 @@ def test_case_combined_mixed_sj(log_results=False):
 # versatility
 def test_case_combined_versatility_pc(log_results=False):
     net = nw.water_combined_versatility()
-    p_diff, v_diff_abs = pipeflow_openmodelica_comparison(net, log_results)
+    p_diff, v_diff_abs = pipeflow_comparison(net, 'om', log_results, friction_model='colebrook')
     assert np.all(p_diff < 0.06)  # only in one place the comparison for 0.01 is not correct
     assert np.all(v_diff_abs < 0.05)
 
 def test_case_combined_versatility_sj(log_results=False):
     net = nw.water_combined_versatility(method="swamee-jain")
-    p_diff, v_diff_abs = pipeflow_openmodelica_comparison(net, log_results)
+    p_diff, v_diff_abs = pipeflow_comparison(net, 'om', log_results, friction_model='colebrook')
     assert np.all(p_diff < 0.06)  # only in one place the comparison for 0.01 is not correct
     assert np.all(v_diff_abs < 0.05)
 
@@ -52,52 +52,52 @@ def test_case_combined_versatility_sj(log_results=False):
 # delta
 def test_case_meshed_delta_pc(log_results=False):
     net = nw.water_meshed_delta()
-    p_diff, v_diff_abs = pipeflow_openmodelica_comparison(net, log_results)
+    p_diff, v_diff_abs = pipeflow_comparison(net, 'om', log_results, friction_model='colebrook')
     assert np.all(p_diff < 0.01)
     assert np.all(v_diff_abs < 0.05)
 
 def test_case_meshed_delta_sj(log_results=False):
     net = nw.water_meshed_delta(method="swamee-jain")
-    p_diff, v_diff_abs = pipeflow_openmodelica_comparison(net, log_results)
+    p_diff, v_diff_abs = pipeflow_comparison(net, 'om', log_results, friction_model='colebrook')
     assert np.all(p_diff < 0.01)
     assert np.all(v_diff_abs < 0.05)
 
 # two_valves
 def test_case_meshed_2valves_pc(log_results=False):
     net = nw.water_meshed_2valves()
-    p_diff, v_diff_abs = pipeflow_openmodelica_comparison(net, log_results)
+    p_diff, v_diff_abs = pipeflow_comparison(net, 'om', log_results, friction_model='colebrook')
     assert np.all(p_diff < 0.01)
     assert np.all(v_diff_abs < 0.05)
 
 def test_case_meshed_2valves_sj(log_results=False):
     net = nw.water_meshed_2valves(method="swamee-jain")
-    p_diff, v_diff_abs = pipeflow_openmodelica_comparison(net, log_results)
+    p_diff, v_diff_abs = pipeflow_comparison(net, 'om', log_results, friction_model='colebrook')
     assert np.all(p_diff < 0.01)
     assert np.all(v_diff_abs < 0.05)
 
 # pumps
 def test_case_meshed_pumps_pc(log_results=False):
     net = nw.water_meshed_pumps()
-    p_diff, v_diff_abs = pipeflow_openmodelica_comparison(net, log_results)
+    p_diff, v_diff_abs = pipeflow_comparison(net, 'om', log_results, friction_model='colebrook')
     assert np.all(p_diff < 0.02)  # in two places the comparison for 0.01 is not correct
     assert np.all(v_diff_abs < 0.05)
 
 def test_case_meshed_pumps_sj(log_results=False):
     net = nw.water_meshed_pumps(method="swamee-jain")
-    p_diff, v_diff_abs = pipeflow_openmodelica_comparison(net, log_results)
+    p_diff, v_diff_abs = pipeflow_comparison(net, 'om', log_results, friction_model='colebrook')
     assert np.all(p_diff < 0.02)  # in two places the comparison for 0.01 is not correct
     assert np.all(v_diff_abs < 0.05)
 
 # heights
 def test_case_meshed_heights_pc(log_results=False):
     net = nw.water_meshed_heights()
-    p_diff, v_diff_abs = pipeflow_openmodelica_comparison(net, log_results)
+    p_diff, v_diff_abs = pipeflow_comparison(net, 'om', log_results, friction_model='colebrook')
     assert np.all(p_diff < 0.01)
     assert np.all(v_diff_abs < 0.05)
 
 def test_case_meshed_heights_sj(log_results=False):
     net = nw.water_meshed_heights(method="swamee-jain")
-    p_diff, v_diff_abs = pipeflow_openmodelica_comparison(net, log_results)
+    p_diff, v_diff_abs = pipeflow_comparison(net, 'om', log_results, friction_model='colebrook')
     assert np.all(p_diff < 0.01)
     assert np.all(v_diff_abs < 0.05)
 
@@ -105,39 +105,39 @@ def test_case_meshed_heights_sj(log_results=False):
 # pipe_1
 def test_case_one_pipe_1_pc(log_results=False):
     net = nw.water_one_pipe1()
-    p_diff, v_diff_abs = pipeflow_openmodelica_comparison(net, log_results)
+    p_diff, v_diff_abs = pipeflow_comparison(net, 'om', log_results, friction_model='colebrook')
     assert np.all(p_diff < 0.01)
     assert np.all(v_diff_abs < 0.05)
 
 def test_case_one_pipe_1_sj(log_results=False):
     net = nw.water_one_pipe1(method="swamee-jain")
-    p_diff, v_diff_abs = pipeflow_openmodelica_comparison(net, log_results)
+    p_diff, v_diff_abs = pipeflow_comparison(net, 'om', log_results, friction_model='colebrook')
     assert np.all(p_diff < 0.01)
     assert np.all(v_diff_abs < 0.05)
 
 # pipe_2
 def test_case_one_pipe_2_pc(log_results=False):
     net = nw.water_one_pipe2()
-    p_diff, v_diff_abs = pipeflow_openmodelica_comparison(net, log_results)
+    p_diff, v_diff_abs = pipeflow_comparison(net, 'om', log_results, friction_model='colebrook')
     assert np.all(p_diff < 0.01)
     assert np.all(v_diff_abs < 0.05)
 
 def test_case_one_pipe_2_sj(log_results=False):
     net = nw.water_one_pipe2(method="swamee-jain")
-    p_diff, v_diff_abs = pipeflow_openmodelica_comparison(net, log_results)
+    p_diff, v_diff_abs = pipeflow_comparison(net, 'om', log_results, friction_model='colebrook')
     assert np.all(p_diff < 0.01)
     assert np.all(v_diff_abs < 0.05)
 
 # pipe_3
 def test_case_one_pipe_3_pc(log_results=False):
     net = nw.water_one_pipe3()
-    p_diff, v_diff_abs = pipeflow_openmodelica_comparison(net, log_results)
+    p_diff, v_diff_abs = pipeflow_comparison(net, 'om', log_results, friction_model='colebrook')
     assert np.all(p_diff < 0.01)
     assert np.all(v_diff_abs < 0.05)
 
 def test_case_one_pipe_3_sj(log_results=False):
     net = nw.water_one_pipe3(method="swamee-jain")
-    p_diff, v_diff_abs = pipeflow_openmodelica_comparison(net, log_results)
+    p_diff, v_diff_abs = pipeflow_comparison(net, 'om', log_results, friction_model='colebrook')
     assert np.all(p_diff < 0.01)
     assert np.all(v_diff_abs < 0.05)
 
@@ -145,52 +145,52 @@ def test_case_one_pipe_3_sj(log_results=False):
 # cross_3ext
 def test_case_strand_net_cross3ext_pc(log_results=False):
     net = nw.water_strand_cross()
-    p_diff, v_diff_abs = pipeflow_openmodelica_comparison(net, log_results)
+    p_diff, v_diff_abs = pipeflow_comparison(net, 'om', log_results, friction_model='colebrook')
     assert np.all(p_diff < 0.01)
     assert np.all(v_diff_abs < 0.05)
 
 def test_case_strand_net_cross3ext_sj(log_results=False):
     net = nw.water_strand_cross(method="swamee-jain")
-    p_diff, v_diff_abs = pipeflow_openmodelica_comparison(net, log_results)
+    p_diff, v_diff_abs = pipeflow_comparison(net, 'om', log_results, friction_model='colebrook')
     assert np.all(p_diff < 0.01)
     assert np.all(v_diff_abs < 0.05)
 
 # strand_net
 def test_case_strand_net_pc(log_results=False):
     net = nw.water_simple_strand_net()
-    p_diff, v_diff_abs = pipeflow_openmodelica_comparison(net, log_results)
+    p_diff, v_diff_abs = pipeflow_comparison(net, 'om', log_results, friction_model='colebrook')
     assert np.all(p_diff < 0.01)
     assert np.all(v_diff_abs < 0.05)
 
 def test_case_strand_net_sj(log_results=False):
     net = nw.water_simple_strand_net(method="swamee-jain")
-    p_diff, v_diff_abs = pipeflow_openmodelica_comparison(net, log_results)
+    p_diff, v_diff_abs = pipeflow_comparison(net, 'om', log_results, friction_model='colebrook')
     assert np.all(p_diff < 0.01)
     assert np.all(v_diff_abs < 0.05)
 
 # two_pipes
 def test_case_strand_net_2pipes_pc(log_results=False):
     net = nw.water_strand_2pipes()
-    p_diff, v_diff_abs = pipeflow_openmodelica_comparison(net, log_results)
+    p_diff, v_diff_abs = pipeflow_comparison(net, 'om', log_results, friction_model='colebrook')
     assert np.all(p_diff < 0.01)
     assert np.all(v_diff_abs < 0.05)
 
 def test_case_strand_net_2pipes_sj(log_results=False):
     net = nw.water_strand_2pipes(method="swamee-jain")
-    p_diff, v_diff_abs = pipeflow_openmodelica_comparison(net, log_results)
+    p_diff, v_diff_abs = pipeflow_comparison(net, 'om', log_results, friction_model='colebrook')
     assert np.all(p_diff < 0.01)
     assert np.all(v_diff_abs < 0.05)
 
 # two_pumps
 def test_case_strand_net_2pumps_pc(log_results=False):
     net = nw.water_strand_net_2pumps()
-    p_diff, v_diff_abs = pipeflow_openmodelica_comparison(net, log_results)
+    p_diff, v_diff_abs = pipeflow_comparison(net, 'om', log_results, friction_model='colebrook')
     assert np.all(p_diff < 0.01)
     assert np.all(v_diff_abs < 0.05)
 
 def test_case_strand_net_2pumps_sj(log_results=False):
     net = nw.water_strand_net_2pumps(method="swamee-jain")
-    p_diff, v_diff_abs = pipeflow_openmodelica_comparison(net, log_results)
+    p_diff, v_diff_abs = pipeflow_comparison(net, 'om', log_results, friction_model='colebrook')
     assert np.all(p_diff < 0.01)
     assert np.all(v_diff_abs < 0.05)
 
@@ -198,26 +198,26 @@ def test_case_strand_net_2pumps_sj(log_results=False):
 # t_cross
 def test_case_tcross_pc(log_results=False):
     net = nw.water_tcross()
-    p_diff, v_diff_abs = pipeflow_openmodelica_comparison(net, log_results)
+    p_diff, v_diff_abs = pipeflow_comparison(net, 'om', log_results, friction_model='colebrook')
     assert np.all(p_diff < 0.01)
     assert np.all(v_diff_abs < 0.05)
 
 def test_case_tcross_sj(log_results=False):
     net = nw.water_tcross(method="swamee-jain")
-    p_diff, v_diff_abs = pipeflow_openmodelica_comparison(net, log_results)
+    p_diff, v_diff_abs = pipeflow_comparison(net, 'om', log_results, friction_model='colebrook')
     assert np.all(p_diff < 0.02) # 2 values are greater than 0.01
     assert np.all(v_diff_abs < 0.05)
 
 # valves
 def test_case_tcross_valves_pc(log_results=False):
     net = nw.water_tcross_valves()
-    p_diff, v_diff_abs = pipeflow_openmodelica_comparison(net, log_results)
+    p_diff, v_diff_abs = pipeflow_comparison(net, 'om', log_results, friction_model='colebrook')
     assert np.all(p_diff < 0.4)  # only in one place the comparison for 0.01 is not correct
     assert np.all(v_diff_abs < 0.05)
 
 def test_case_tcross_valves_sj(log_results=False):
     net = nw.water_tcross_valves(method="swamee-jain")
-    p_diff, v_diff_abs = pipeflow_openmodelica_comparison(net, log_results)
+    p_diff, v_diff_abs = pipeflow_comparison(net, 'om', log_results, friction_model='colebrook')
     assert np.all(p_diff < 0.4)  # only in one place the comparison for 0.01 is not correct
     assert np.all(v_diff_abs < 0.05)
 
@@ -225,13 +225,13 @@ def test_case_tcross_valves_sj(log_results=False):
 # two_pipes
 def test_case_2eg_two_pipes_pc(log_results=False):
     net = nw.water_2eg_two_pipes()
-    p_diff, v_diff_abs = pipeflow_openmodelica_comparison(net, log_results)
+    p_diff, v_diff_abs = pipeflow_comparison(net, 'om', log_results, friction_model='colebrook')
     assert np.all(p_diff < 0.01)
     assert np.all(v_diff_abs < 0.05)
 
 def test_case_2eg_two_pipes_sj(log_results=False):
     net = nw.water_2eg_two_pipes(method="swamee-jain")
-    p_diff, v_diff_abs = pipeflow_openmodelica_comparison(net, log_results)
+    p_diff, v_diff_abs = pipeflow_comparison(net, 'om', log_results, friction_model='colebrook')
     assert np.all(p_diff < 0.01)
     assert np.all(v_diff_abs < 0.05)
 
--- pandapipes/test/comparison/stanet_comparison/test_gas_stanet.py
+++ pandapipes/test/comparison/stanet_comparison/test_gas_stanet.py
@@ -6,7 +6,7 @@ import numpy as np
 import pandapipes.networks.simple_gas_networks as nw
 import pytest
 from pandapipes.pipeflow import logger as pf_logger
-from pandapipes.test.stanet_comparison.pipeflow_stanet_comparison import pipeflow_stanet_comparison
+from pandapipes.test.comparison.pipeflow_comparison import pipeflow_comparison
 
 try:
     import pplog as logging
@@ -29,7 +29,7 @@ def test_case_3parallel_n(log_results=False):
     :rtype:
     """
     net = nw.gas_3parallel(method="n")
-    p_diff, v_diff_abs = pipeflow_stanet_comparison(net, log_results)
+    p_diff, v_diff_abs = pipeflow_comparison(net, 'stanet',log_results)
     assert np.all(p_diff < 0.002)
     assert np.all(v_diff_abs < 0.03)
 
@@ -44,7 +44,7 @@ def test_case_combined_3parallel_pc(log_results=False):
     :rtype:
     """
     net = nw.gas_3parallel(method="pc")
-    p_diff, v_diff_abs = pipeflow_stanet_comparison(net, log_results, friction_model="colebrook")
+    p_diff, v_diff_abs = pipeflow_comparison(net, 'stanet', log_results, friction_model="colebrook")
     assert np.all(p_diff < 0.002)
     assert np.all(v_diff_abs < 0.03)
 
@@ -60,7 +60,7 @@ def test_case_square_n(log_results=False):
     :rtype:
     """
     net = nw.gas_meshed_square(method="n")
-    p_diff, v_diff_abs = pipeflow_stanet_comparison(net, log_results)
+    p_diff, v_diff_abs = pipeflow_comparison(net, 'stanet', log_results)
     assert np.all(p_diff < 0.002)
     assert np.all(v_diff_abs < 0.03)
 
@@ -75,7 +75,7 @@ def test_case_square_pc(log_results=False):
     :rtype:
     """
     net = nw.gas_meshed_square(method="pc")
-    p_diff, v_diff_abs = pipeflow_stanet_comparison(net, log_results, friction_model="colebrook")
+    p_diff, v_diff_abs = pipeflow_comparison(net, 'stanet', log_results, friction_model="colebrook")
     assert np.all(p_diff < 0.002)
     assert np.all(v_diff_abs < 0.03)
 
@@ -90,7 +90,7 @@ def test_case_meshed_delta_pc(log_results=False):
     :rtype:
     """
     net = nw.gas_meshed_delta()
-    p_diff, v_diff_abs = pipeflow_stanet_comparison(net, log_results, friction_model="colebrook")
+    p_diff, v_diff_abs = pipeflow_comparison(net, 'stanet', log_results, friction_model="colebrook")
     assert np.all(p_diff < 0.002)
     assert np.all(v_diff_abs < 0.03)
 
@@ -105,7 +105,7 @@ def test_case_meshed_pumps(log_results=False):
     :rtype:
     """
     net = nw.gas_meshed_pumps()
-    p_diff, v_diff_abs = pipeflow_stanet_comparison(net, log_results)
+    p_diff, v_diff_abs = pipeflow_comparison(net, 'stanet', log_results)
     assert np.all(p_diff < 0.002)
     assert np.all(v_diff_abs < 0.03)
 
@@ -113,7 +113,7 @@ def test_case_meshed_pumps(log_results=False):
 # two_valves_N
 def test_case_meshed_2valves_n(log_results=False):
     net = nw.gas_meshed_two_valves(method="n")
-    p_diff, v_diff_abs = pipeflow_stanet_comparison(net, log_results)
+    p_diff, v_diff_abs = pipeflow_comparison(net, 'stanet', log_results)
     assert np.all(p_diff < 0.001)
     assert np.all(v_diff_abs < 0.001)
 
@@ -121,7 +121,7 @@ def test_case_meshed_2valves_n(log_results=False):
 # two_valves_PC
 def test_case_meshed_2valves_pc(log_results=False):
     net = nw.gas_meshed_two_valves(method="pc")
-    p_diff, v_diff_abs = pipeflow_stanet_comparison(net, log_results, friction_model="colebrook")
+    p_diff, v_diff_abs = pipeflow_comparison(net, 'stanet', log_results, friction_model="colebrook")
     assert np.all(p_diff < 0.001)
     assert np.all(v_diff_abs < 0.001)
 
@@ -137,7 +137,7 @@ def test_case_one_pipe1_n(log_results=False):
     :rtype:
     """
     net = nw.gas_one_pipe1(method="n")
-    p_diff, v_diff_abs = pipeflow_stanet_comparison(net, log_results)
+    p_diff, v_diff_abs = pipeflow_comparison(net, 'stanet', log_results)
     assert np.all(p_diff < 0.002)
     assert np.all(v_diff_abs < 0.03)
 
@@ -152,7 +152,7 @@ def test_case_one_pipe1_pc(log_results=False):
     :rtype:
     """
     net = nw.gas_one_pipe1(method="pc")
-    p_diff, v_diff_abs = pipeflow_stanet_comparison(net, log_results, friction_model="colebrook")
+    p_diff, v_diff_abs = pipeflow_comparison(net, 'stanet', log_results, friction_model="colebrook")
     assert np.all(p_diff < 0.002)
     assert np.all(v_diff_abs < 0.03)
 
@@ -167,7 +167,7 @@ def test_case_one_pipe2_n(log_results=False):
     :rtype:
     """
     net = nw.gas_one_pipe2(method="n")
-    p_diff, v_diff_abs = pipeflow_stanet_comparison(net, log_results)
+    p_diff, v_diff_abs = pipeflow_comparison(net, 'stanet', log_results)
     assert np.all(p_diff < 0.002)
     assert np.all(v_diff_abs < 0.03)
 
@@ -182,7 +182,7 @@ def test_case_one_pipe2_pc(log_results=False):
     :rtype:
     """
     net = nw.gas_one_pipe2(method="pc")
-    p_diff, v_diff_abs = pipeflow_stanet_comparison(net, log_results, friction_model="colebrook")
+    p_diff, v_diff_abs = pipeflow_comparison(net, 'stanet', log_results, friction_model="colebrook")
     assert np.all(p_diff < 0.002)
     assert np.all(v_diff_abs < 0.03)
 
@@ -198,7 +198,7 @@ def test_case_strand_2pipes_n(log_results=False):
     :rtype:
     """
     net = nw.gas_strand_2pipes(method="n")
-    p_diff, v_diff_abs = pipeflow_stanet_comparison(net, log_results)
+    p_diff, v_diff_abs = pipeflow_comparison(net, 'stanet', log_results)
     assert np.all(p_diff < 0.002)
     assert np.all(v_diff_abs < 0.03)
 
@@ -213,7 +213,7 @@ def test_case_strand_2pipes_pc(log_results=False):
     :rtype:
     """
     net = nw.gas_strand_2pipes(method="pc")
-    p_diff, v_diff_abs = pipeflow_stanet_comparison(net, log_results, friction_model="colebrook")
+    p_diff, v_diff_abs = pipeflow_comparison(net, 'stanet', log_results, friction_model="colebrook")
     assert np.all(p_diff < 0.002)
     assert np.all(v_diff_abs < 0.03)
 
@@ -228,7 +228,7 @@ def test_case_strand_pump(log_results=False):
     :rtype:
     """
     net = nw.gas_strand_pump()
-    p_diff, v_diff_abs = pipeflow_stanet_comparison(net, log_results)
+    p_diff, v_diff_abs = pipeflow_comparison(net, 'stanet', log_results)
     assert np.all(p_diff < 0.002)
     assert np.all(v_diff_abs < 0.03)
 
@@ -244,7 +244,7 @@ def test_case_tcross1_n(log_results=False):
     :rtype:
     """
     net = nw.gas_tcross1(method="n")
-    p_diff, v_diff_abs = pipeflow_stanet_comparison(net, log_results)
+    p_diff, v_diff_abs = pipeflow_comparison(net, 'stanet', log_results)
     assert np.all(p_diff < 0.002)
     assert np.all(v_diff_abs < 0.03)
 
@@ -259,7 +259,7 @@ def test_case_tcross1_pc(log_results=False):
     :rtype:
     """
     net = nw.gas_tcross1(method="pc")
-    p_diff, v_diff_abs = pipeflow_stanet_comparison(net, log_results, friction_model="colebrook")
+    p_diff, v_diff_abs = pipeflow_comparison(net, 'stanet', log_results, friction_model="colebrook")
     assert np.all(p_diff < 0.002)
     assert np.all(v_diff_abs < 0.03)
 
@@ -274,7 +274,7 @@ def test_case_tcross2_n(log_results=False):
     :rtype:
     """
     net = nw.gas_tcross2(method="n")
-    p_diff, v_diff_abs = pipeflow_stanet_comparison(net, log_results)
+    p_diff, v_diff_abs = pipeflow_comparison(net, 'stanet', log_results)
     assert np.all(p_diff < 0.002)
     assert np.all(v_diff_abs < 0.03)
 
@@ -289,7 +289,7 @@ def test_case_tcross2_pc(log_results=False):
     :rtype:
     """
     net = nw.gas_tcross2(method="pc")
-    p_diff, v_diff_abs = pipeflow_stanet_comparison(net, log_results, friction_model="colebrook")
+    p_diff, v_diff_abs = pipeflow_comparison(net, 'stanet', log_results, friction_model="colebrook")
     assert np.all(p_diff < 0.002)
     assert np.all(v_diff_abs < 0.03)
 
@@ -305,7 +305,7 @@ def test_case_2eg_hnet_n(log_results=False):
     :rtype:
     """
     net = nw.gas_2eg_hnet(method="n")
-    p_diff, v_diff_abs = pipeflow_stanet_comparison(net, log_results)
+    p_diff, v_diff_abs = pipeflow_comparison(net, 'stanet', log_results)
     assert np.all(p_diff < 0.002)
     assert np.all(v_diff_abs < 0.03)
 
@@ -320,7 +320,7 @@ def test_case_2eg_hnet_pc(log_results=False):
     :rtype:
     """
     net = nw.gas_2eg_hnet(method="pc")
-    p_diff, v_diff_abs = pipeflow_stanet_comparison(net, log_results, friction_model="colebrook")
+    p_diff, v_diff_abs = pipeflow_comparison(net, 'stanet', log_results, friction_model="colebrook")
     assert np.all(p_diff < 0.002)
     assert np.all(v_diff_abs < 0.03)
 
